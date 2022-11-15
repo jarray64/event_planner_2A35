c7fa9cdb0661bad56de287ba187642153d9b841d
jarray64 <abdelmonam.jarray@esprit.tn>
2022-11-15 12:37:01 +0100

    last edit

---
+++ projetevent/anglais.ts
@@ -0,0 +1,284 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US" sourcelanguage="fr_FR">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.ui" line="14"/>
+        <source>E-event</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="51"/>
+        <source>login</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="91"/>
+        <source>log in</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="135"/>
+        <source>Username</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="183"/>
+        <source>password</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="218"/>
+        <source>password incorrect , try again . </source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="evenement.cpp" line="46"/>
+        <source>ID</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="evenement.cpp" line="47"/>
+        <source>PLACE</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="evenement.cpp" line="48"/>
+        <source>TYPE</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="evenement.cpp" line="49"/>
+        <source>DATE</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="evenement.cpp" line="50"/>
+        <source>CINCLIENT</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>Smtp</name>
+    <message>
+        <location filename="smtp.cpp" line="53"/>
+        <source>Qt Simple SMTP client</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="smtp.cpp" line="53"/>
+        <source>Couldn&apos;t open the file
+
+</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="smtp.cpp" line="234"/>
+        <source>Message sent</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="smtp.cpp" line="246"/>
+        <source>Failed to send message</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>gestien</name>
+    <message>
+        <location filename="gestien.ui" line="14"/>
+        <source>E-event</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="gestien.ui" line="50"/>
+        <source>evenements</source>
+        <translation>Events</translation>
+    </message>
+    <message>
+        <location filename="gestien.ui" line="80"/>
+        <source>marketing</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="gestien.ui" line="110"/>
+        <source>partenaires</source>
+        <translation>Partners</translation>
+    </message>
+    <message>
+        <location filename="gestien.ui" line="139"/>
+        <source>employés</source>
+        <translation>Employers</translation>
+    </message>
+    <message>
+        <location filename="gestien.ui" line="168"/>
+        <source>clients</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="gestien.ui" line="215"/>
+        <source>Label</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="gestien.ui" line="409"/>
+        <source>affichage</source>
+        <translation>Display</translation>
+    </message>
+    <message>
+        <location filename="gestien.ui" line="466"/>
+        <source>Supprimer</source>
+        <translation>Delete</translation>
+    </message>
+    <message>
+        <location filename="gestien.ui" line="502"/>
+        <source>pdf</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="gestien.ui" line="538"/>
+        <source>refresh</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="gestien.ui" line="574"/>
+        <source>excel</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="gestien.ui" line="610"/>
+        <source>map</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="gestien.ui" line="646"/>
+        <source>mail</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="gestien.ui" line="674"/>
+        <source>Rechercher</source>
+        <extracomment>Recherche</extracomment>
+        <translation>search</translation>
+    </message>
+    <message>
+        <location filename="gestien.ui" line="709"/>
+        <source>Trier</source>
+        <translation>Sort</translation>
+    </message>
+    <message>
+        <location filename="gestien.ui" line="779"/>
+        <source>Ajouter/modifier evenement</source>
+        <translation>ADD/Edit event</translation>
+    </message>
+    <message>
+        <location filename="gestien.ui" line="812"/>
+        <source>ID</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="gestien.ui" line="840"/>
+        <source>PLACE</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="gestien.ui" line="880"/>
+        <source>Ajouter</source>
+        <translation>Add</translation>
+    </message>
+    <message>
+        <location filename="gestien.ui" line="920"/>
+        <source>modifier</source>
+        <translation>Edit</translation>
+    </message>
+    <message>
+        <location filename="gestien.ui" line="949"/>
+        <source>mariage</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="gestien.ui" line="954"/>
+        <source>sport</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="gestien.ui" line="959"/>
+        <source>meet</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="gestien.ui" line="964"/>
+        <source>buisness</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="gestien.ui" line="969"/>
+        <source>autre</source>
+        <translation>other</translation>
+    </message>
+    <message>
+        <location filename="gestien.ui" line="1044"/>
+        <source>recipient</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="gestien.ui" line="1062"/>
+        <source>type your message here ...</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="gestien.ui" line="1079"/>
+        <source>attachement here</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="gestien.ui" line="1096"/>
+        <source>subject</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="gestien.ui" line="1113"/>
+        <source>Browse ...</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="gestien.ui" line="1133"/>
+        <source>your mail password</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="gestien.ui" line="1151"/>
+        <source>Send</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="gestien.ui" line="1184"/>
+        <source>addimage</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="gestien.ui" line="1199"/>
+        <source>{8856F961-340A-11D0-A96B-00C04FD705A2}</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="gestien.ui" line="1299"/>
+        <source>admin</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="gestien.cpp" line="659"/>
+        <source>Excel file</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="gestien.cpp" line="660"/>
+        <source>Excel Files (*.xls)</source>
+        <translation></translation>
+    </message>
+</context>
+</TS>
--- projetevent/main.cpp
+++ projetevent/main.cpp
@@ -2,14 +2,26 @@
 #include "conncetion.h"
 #include "QMessageBox"
 #include <QApplication>
-
+#include "QTranslator"
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
     Connection c;
    c.createconnect();
+   QTranslator T;
+   QStringList languages;
+         languages <<"French"<<"English";
+         const QString lang=QInputDialog::getItem(NULL,"Select language ","Language",languages);
+         if (lang=="English")
 
+       {
+            T.load(":/new/traduction/anglais.qm");
+       }
+       if (lang !="French")
+       {
+           a.installTranslator(&T);
+       }
     w.show();
 
     return a.exec();
